42b6e99cbdb31e7b96cefc5f55f5a824499993ab
Unknown <ki575@nyu.edu>
2017-10-10 09:20:47 -0400

    Win Text Added

---
+++ Assets/winScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class winScript : MonoBehaviour {
+    public Text myText;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter()
+    {
+        myText.text = "WIN";
+    }
+}
